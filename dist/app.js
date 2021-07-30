60c800ea161bec0e2b890854ea2859e7dd36c95a
Russ <rperry1886@gmail.com>
2021-07-30 00:24:54 -0400

    Add responsive content sections

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,12 @@
 const guideImage = document.getElementById('guide');
 const contentTextDivs = document.querySelectorAll('.content-text');
-console.log(guideImage.offsetHeight);
+console.log(guideImage.clientHeight);
 
 function resizeContent() {
-    let guideImageHeight = guideImage.offsetHeight;
-    contentTextDivs.forEach((singleDiv) => {
-        singleDiv.style.height = `${guideImageHeight}px`;
-    });
+  let guideImageHeight = guideImage.offsetHeight;
+  contentTextDivs.forEach((singleDiv) => {
+    singleDiv.style.height = `${guideImageHeight}px`;
+  });
 }
 
 resizeContent();
