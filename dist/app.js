4e2321bd2a73693508cc36cb4be8a833af0580f1
rperry99 <rperry1886@gmail.com>
2021-08-04 23:56:22 -0400

    Remove aria hidden and clean up js file

--- dist/app.js
+++ dist/app.js
@@ -1,19 +1,3 @@
-// // Toggle Nav section on and off on mobile.
-// const mobileNav = document.getElementById('nav-mobile-links');
-// const hamburger = document.getElementById('hamburger');
-// const htmlBody = document.getElementById('page-html');
-
-// console.log(mobileNav.style);
-// function toggleNav() {
-//   if (mobileNav.style.display == '') {
-//     mobileNav.style.display = 'flex';
-//     htmlBody.classList.add('stop-scrolling');
-//   } else {
-//     htmlBody.classList.remove('stop-scrolling');
-//     mobileNav.style.display = '';
-//   }
-// }
-
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.querySelector('.nav-links');
 const htmlBody = document.querySelector('#page-html');
